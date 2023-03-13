d505f8110bac22d0ea5982dad7e17e6ff2835c62
shang906 <jiashang@ucdavis.edu>
2023-03-13 12:35:38 -0700

    dairy

---
+++ final/version1/script.js
@@ -0,0 +1,64 @@
+window.addEventListener('load', function () {
+    'use strict';
+    console.log("reading js");
+
+   
+    const openBtns = document.querySelectorAll('.open');
+    console.log(openBtns);
+    const closeBtns = document.querySelectorAll('.close');
+
+    const sliderContent = document.querySelector('.a');
+    const sliderWidth = sliderContent.offsetWidth;
+    const cloned = sliderContent.cloneNode(true);
+    cloned.className = "b";
+    document.querySelector('.slider').appendChild(cloned);
+    let root = document.querySelector(':root');
+    const endLeftPos = `-${sliderWidth}px`;
+
+
+
+    //slide effect/
+        
+    root.style.setProperty('--sliderwidth', endLeftPos);
+    console.log(getComputedStyle(root).getPropertyValue('--sliderwidth'));
+    document.querySelector('.slider').classList.add("animate");
+
+    document.querySelector('.slider').addEventListener('mouseover', function(){
+        document.querySelector('.animate').style.animationPlayState = 'paused';
+    });
+
+    document.querySelector('.slider').addEventListener('mouseout', function(){
+        document.querySelector('.animate').style.animationPlayState = 'running';
+    });
+
+    
+
+    for (const eachBtn of openBtns) {
+        eachBtn.addEventListener('click', function(event) {
+            event.preventDefault();
+            const thisBtn = this.id;
+            console.log(thisBtn);
+            if (document.getElementById(`ol-${thisBtn}`).classList.contains('overlay-wide')) {
+                document.getElementById(`ol-${thisBtn}`).className = `overlay-wide showing`;
+            } else {
+                document.getElementById(`ol-${thisBtn}`).className = `overlay-tall showing`;
+            }
+
+
+        });
+    }
+
+    for (const eachBtn of closeBtns) {
+        eachBtn.addEventListener('click', function(event) {
+            event.preventDefault();
+            document.querySelector('.showing').className = 'overlay hidden';
+        });
+    }
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'Escape') {
+            document.querySelector('.showing').className = 'overlay hidden';
+        }
+    });
+
+});
